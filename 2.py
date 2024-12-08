0cf7764184e241c225ef5b878cbf27493b08c263
Math1111 <emelyanov2704@mail.ru>
2024-12-08 12:12:17 +0300

    +

--- 2.py
+++ 2.py
@@ -10,6 +10,10 @@ while a!=0:
         if a>ma:
             ma=a
     a=int(input())
+if 99>mi<1000 or mi%100!=11:
+    mi=ma
+if 99>ma<1000 or ma%100!=11:
+    ma=mi
 if k!=0:
     print('Минимальное:', mi)
     print('Максимальное:', ma)
