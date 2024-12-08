99e2da9b728c7d3cc3aa373f8005bd7b79a3f70e
Math1111 <emelyanov2704@mail.ru>
2024-12-08 18:07:48 +0300

    +

--- 1.py
+++ 1.py
@@ -1,19 +1,19 @@
 a=int(input())
-mi=a
-ma=a
+mi=0
+ma=0
 k=0
+kmima=0
 while a!=0:
-    if a%7!=0:
+    if (mi==0 or ma==0) and a%7!=0:
+        mi=a
+        ma=a
         k+=1
+    if a%7!=0:
         if a<mi:
             mi=a
         if a>ma:
             ma=a
     a=int(input())
-if mi%7==0:
-    mi=ma
-if ma%7==0:
-    ma=mi
 if k!=0:
     print('Минимальное:', mi)
     print('Максимальное:', ma)
--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 a=int(input())
-mi=a
-ma=a
+mi=1000
+ma=99
 k=0
 while a!=0:
     if 99<a<1000 and a%100==11:
@@ -10,10 +10,6 @@ while a!=0:
         if a>ma:
             ma=a
     a=int(input())
-if 99>mi<1000 or mi%100!=11:
-    mi=ma
-if 99>ma<1000 or ma%100!=11:
-    ma=mi
 if k!=0:
     print('Минимальное:', mi)
     print('Максимальное:', ma)
